57d7eabf27a54fb735f9e4138d30b6192233abba
Bez Hermoso <bez@activelamp.com>
2014-09-15 11:27:03 -0700

    Working version.

---
+++ README.md
@@ -0,0 +1,4 @@
+# activelamp_composer
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'activelamp_composer'
+maintainer       'Bez Hermoso, ActiveLAMP'
+maintainer_email 'bez@activelamp.com'
+license          'Apache 2.0'
+description      'Installs/Configures activelamp_composer'
+long_description 'Installs/Configures activelamp_composer'
+version          '0.1.0'
+
