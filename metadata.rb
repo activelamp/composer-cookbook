a83e43ddafcd3929028efb8336aa5a35808406a7
Bez Hermoso <bez@activelamp.com>
2014-09-18 14:15:34 -0700

    Versioning.

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ license          'Apache 2.0'
 description      'Provides the `composer` resource.'
 long_description 'Provides the `composer` resource that can interact with Composer projects.'
 version          '0.0.1'
-
